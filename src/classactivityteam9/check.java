648cbdaa9641ea3cb01e3f4757ca0d53d5d4890a
vinay <vinayraodugyala99@gmail.com>
2019-02-10 09:15:38 -0500

    check and credit created

---
+++ src/classactivityteam9/check.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classactivityteam9;
+
+/**
+ *
+ * @author temp
+ */
+public class check
+{
+    String name;
+    String bankid;
+    
+    public void author()
+    {
+        System.out.println("athorization");
+    }
+    
+}
---
+++ src/classactivityteam9/credit.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classactivityteam9;
+import java.util.*;
+/**
+ *
+ * @author temp
+ */
+public class credit 
+{
+    String name;
+    String type;
+    String expdate;
+    
+    public void author()
+    {
+        System.out.println("Authorization of person");
+    }
+}
+
